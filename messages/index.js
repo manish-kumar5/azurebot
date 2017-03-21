eb7b5e112250d4f1856075d292b43485e3b4cd43
Kumar <manish-kumar5@mercer.com>
2017-03-21 14:36:55 +0530

    hehehe

--- messages/index.js
+++ messages/index.js
@@ -99,7 +99,7 @@ var historyTemplate = compile(
 
 var useEmulator = (process.env.NODE_ENV == 'development');
 
-useEmulator = true;
+//useEmulator = true;
 
 var connector = useEmulator ? new builder.ChatConnector() : new botbuilder_azure.BotServiceConnector({
     appId: process.env['MicrosoftAppId'],
