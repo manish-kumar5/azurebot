4f445e2fb23fe39adc207dc24ff8ee290154d3e4
Pankaj Sabharwal <pankaj.sabharwal@mercer.com>
2017-03-29 17:43:33 +0530

    UI fixes and workflow reorder

--- messages/payments/paymentquery.js
+++ messages/payments/paymentquery.js
@@ -14,13 +14,13 @@ module.exports = [
         }
         else {
             //var userInputForm = `<b><p>I am BEN, your AI support representative. What can I help you with today?</p></b> <br> Note: You can choose from below options or type your question in the input box<br>`;
-            msg += 'I will be happy to assist. First, for verification, may I please have your details<br>';
-            msg += `<div class ="form-group"><label for="uid">User Name: </label><input type="text" class ="form-control" id="uid"></div>`;
+            msg += '<p>I will be happy to assist. First, for verification, may I please have your details<br></p>';
+            msg += `<div class ="form-group form-group-sm"><label class="col-sm-2 control-label" for="uid">User Name: </label><input type="text" class ="form-control" id="uid"></div>`;
             // msg += `<div class ="form-group"><label for="pwd">Password : </label><input type="password" class ="form-control" id="pwd"></div>`;
-            msg += `<div class ="form-group"><label for="dob">Date of Birth (MM-DD-YYYY): </label><input type="text" class ="form-control" id="dob"></div>`;
-            msg += `<div class ="form-group"><label for="addr">Address: </label><textarea class="form-control" rows="5" id="addr"></textarea></div>`;
-            msg += `<div class ="form-group"><label for="tele">Telephone: </label><input type="text" class ="form-control" id="tele"></div>`;
-            msg += `<div class ="form-group"><label for="ssn">SSN: </label><input type="text" class ="form-control" id="ssn"></div>`;
+            msg += `<div class ="form-group form-group-sm"><label class="col-sm-2 control-label" for="dob">Date of Birth (MM-DD-YYYY): </label><input type="text" class ="form-control" id="dob"></div>`;
+            //msg += `<div class ="form-group form-group-sm"><label class="col-sm-2 control-label" for="addr">Address: </label><textarea class="form-control" rows="5" id="addr"></textarea></div>`;
+            msg += `<div class ="form-group form-group-sm"><label class="col-sm-2 control-label" for="tele">Telephone: </label><input type="text" class ="form-control" id="tele"></div>`;
+            msg += `<div class ="form-group form-group-sm"><label class="col-sm-2 control-label" for="ssn">SSN (Last 4 digits): </label><input type="text" class ="form-control" id="ssn" maxlength="4"></div>`;
             msg += `<button type="button" class="btn btn-primary" id="btnSubmit" onclick="submitUserInput();">Submit</button>`;
             msg += `<button type="button" class="btn btn-primary" id="btnCancel" onclick="cancelUserInput();>Cancel</button>`;
             msg += `<div id="lblmsg"></div>`;
