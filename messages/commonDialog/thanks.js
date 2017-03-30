0baafc3804314aa9df2410a0223f12776bfdfd66
Kumar <manish-kumar5@mercer.com>
2017-03-30 14:39:21 +0530

    changed star code

--- messages/commonDialog/thanks.js
+++ messages/commonDialog/thanks.js
@@ -17,7 +17,21 @@ module.exports = [
                     "contentType": "application/vnd.microsoft.card.hero",
                     "content": {
                         "title": "Feedback",
-                        "subtitle": session.userData.username + ', Please rate me on information provided to you? <br> <div class="row lead"><div id="stars" class="starrr"></div>        You gave a rating of <span id="count">0</span> star(s)</div><br>',
+                        //"subtitle": session.userData.username + ', Please rate me on information provided to you? <br> <div class="row lead"><div id="stars" class="starrr"></div>        You gave a rating of <span id="count">0</span> star(s)</div><br>',
+                        "subtitle": session.userData.username + `, Please rate me on information provided to your? <br> <div class="stars">
+	  <form action="">
+	    <input class="star star-5" id="star-5" type="radio" name="star"/>
+	    <label class="star star-5" for="star-5"></label>
+	    <input class="star star-4" id="star-4" type="radio" name="star"/>
+	    <label class="star star-4" for="star-4"></label>
+	    <input class="star star-3" id="star-3" type="radio" name="star"/>
+	    <label class="star star-3" for="star-3"></label>
+	    <input class="star star-2" id="star-2" type="radio" name="star"/>
+	    <label class="star star-2" for="star-2"></label>
+	    <input class="star star-1" id="star-1" type="radio" name="star"/>
+	    <label class="star star-1" for="star-1"></label>
+	  </form>
+	</div>`,
                         "buttons": [
                             {
                                 "type": "postBack",
