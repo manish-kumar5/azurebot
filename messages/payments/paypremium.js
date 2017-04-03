9709c248362bbbd19979486c28966762ab3a68d8
Kumar <manish-kumar5@mercer.com>
2017-04-03 13:13:30 +0530

    payment changes

--- messages/payments/paypremium.js
+++ messages/payments/paypremium.js
@@ -5,33 +5,42 @@ module.exports = [
     function (session, args, next) {
         session.send('I am able to accept a one time payment with a Visa or MasterCard.');
 
-        if (!session.dialogData.card.number) {
-            builder.Prompts.text(session, 'What is your card number and the expiration date please');
+        if (!session.dialogData.cardnumber) { // && session.dialogData.card.number
+            builder.Prompts.text(session, 'Please provide your card number');
         }
+    },
+    function (session, results) {
+        if (results.response) {
 
-        next();
-    },
-    function (session, results, next) {
-        if (results.response) {
-            session.dialogData.car.number = result.response;
-            next();
+            session.dialogData.cardnumber = results.response;
+            builder.Prompts.text(session, 'Please provide card expiration date');
+            //next();
         }
     },
-    function (session, results, next) {
+    function (session, results) {
         if (results.response) {
-            session.dialogData.card.expirydate = result.response;
+            session.dialogData.card_expirydate = results.response;
 
             builder.Prompts.text(session, 'Thank you. The current amount due for December 1, 2016 is $28.48, do I have your permission to process this one time payment?');
-            next();
+            //next();
         }
     },
-    function (session, result, next) {
-        if (result.response) {
+    function (session, results, next) {
+        if (results.response) {
 
             if (results.response.toLowerCase() == "yes") {
-                session.send('Thank you. Your payment was approved. Your confirmation number is xxxxxx.');
-                session.endDialogWithResult({ response: session.dialogData.card });
+                builder.Prompts.text(session, 'Thank you. Your payment was approved. Your confirmation number is xxxxxx. <br><b> Do you have any further query?</b>');
+
+            }else{
+                builder.Prompts.text(session, 'Do you have any further query?');
             }
+        }
+    },
+    function(session, results){
+        if(results.response.toLowerCase() == "yes"){
+            session.beginDialog('paymentoptionsquery');
+        }else if(results.response.toLowerCase() == "no"){
+            session.beginDialog('thanks');
         }
     }
 ];
