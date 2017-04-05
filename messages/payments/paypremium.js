cd9d9765d26d6a7e7c3265ca4bbf331af3727bb6
Pankaj Sabharwal <pankaj.sabharwal@mercer.com>
2017-04-05 15:09:49 +0530

    Added Payment details user form

--- messages/payments/paypremium.js
+++ messages/payments/paypremium.js
@@ -3,27 +3,43 @@ var store = require('../store');
 
 module.exports = [
     function (session, args, next) {
-        //session.send('I am able to accept a one time payment with a Visa or MasterCard.');
+        var msg = `
+                <div class="container" id="paymentDetails">
+                    <div class="form-group">
+                        <div class="col-sm-12">
+                            <label class="control-label">I am able to accept a one time payment with a Visa or MasterCard.Plese provide your card number and the expiration date <br></label>
+                        </div>
+                    </div>
 
-        if (!session.dialogData.cardnumber) { // && session.dialogData.card.number
-            builder.Prompts.text(session, 'I am able to accept a one time payment with a Visa or MasterCard. <br> <b> Please provide your card number</b>');
-        }
-    },
-    function (session, results) {
-        if (results.response) {
+                    <div class="form-group">
+                        <div class="col-sm-12">
+                            <input type="text" maxlength=16 class="form-control input-sm change-width"  id="cardnumber" required placeholder="Credit Card Number">
+                        </div>
+                    </div>
 
-            session.dialogData.cardnumber = results.response;
-            builder.Prompts.text(session, 'Please provide card expiration date');
-            //next();
-        }
-    },
-    function (session, results) {
-        if (results.response) {
-            session.dialogData.card_expirydate = results.response;
+                    <div class="form-group">
+                        <div class="col-sm-12">
+                            <input type="text" class ="form-control input-sm change-width" id="expirationmonth" placeholder="Card Expiration Month(MM)" maxlength=2> /
+                            <input type="text" class ="form-control input-sm change-width" id="expirationyear" placeholder="Card Expiration Year(YY)" maxlength=2>
+                        </div>
+                    </div>
 
-            builder.Prompts.text(session, 'Thank you. The current amount due for December 1, 2016 is $28.48, do I have your permission to process this one time payment?');
-            //next();
-        }
+                    <div class="form-group">
+                        <div class="col-sm-12">
+                            <br />
+                            <button type="button" class="btn btn-primary btn-sm" id="btnSubmit" onclick="submitPaymentInput();">Submit</button>&nbsp; &nbsp;
+                            <button type="button" class="btn btn-primary btn-sm" id="btnClear" onclick="clearPaymentInput();">Clear</button>
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <div class="col-sm-12">
+                            <font color="red"><div id="lblpaymentmsg"></div></font>
+                        </div>
+                    </div>
+                </div>`;
+
+        builder.Prompts.text(session, msg);
     },
     function (session, results, next) {
         if (results.response) {
@@ -31,15 +47,15 @@ module.exports = [
             if (results.response.toLowerCase() == "yes") {
                 builder.Prompts.text(session, 'Thank you. Your payment was approved. Your confirmation number is xxxxxx. <br><b> Do you have any further query?</b>');
 
-            }else{
+            } else {
                 builder.Prompts.text(session, 'Do you have any further query?');
             }
         }
     },
-    function(session, results){
-        if(results.response.toLowerCase() == "yes"){
+    function (session, results) {
+        if (results.response.toLowerCase() == "yes") {
             session.beginDialog('paymentoptionsquery');
-        }else if(results.response.toLowerCase() == "no"){
+        } else if (results.response.toLowerCase() == "no") {
             session.beginDialog('thanks');
         }
     }
