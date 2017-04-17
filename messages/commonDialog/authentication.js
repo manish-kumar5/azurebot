c4b3d8f7e40744f77ca940d466394313ebdd9f0c
Kumar <manish-kumar5@mercer.com>
2017-04-17 15:48:36 +0530

    test

--- messages/commonDialog/authentication.js
+++ messages/commonDialog/authentication.js
@@ -6,9 +6,8 @@ module.exports = [
             <!--<form role="form">-->
                 <div class="container" id="authDetails">
                     <div class="form-group">
-                        <div class="col-sm-4">
+
                             <p>I will be happy to assist. First, for verification, may I please have your details</p><br>
-                        </div>
                     </div>
 
                     <div class="form-group">
